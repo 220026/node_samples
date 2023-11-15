f7bae92ffb37cd5caddd6a10a00130c612f5da52
220026 <220026pp@yse-c.net>
2023-11-15 11:36:50 +0900

    add

--- express_sample/models/User.js
+++ express_sample/models/User.js
@@ -5,19 +5,19 @@ const db = require('../lib/db')
 class User {
     //ユーザ追加（非同期処理）
     add = async (post) => {
-        //SQL
-        var sql = `INSERT INTO users SET ?;`
         //DB接続
-        //SQL実行
+        var result;
         try {
             const con = await mysql.createConnection(db.info);
-            var reulst;
+            //SQL実行
+            var sql = `INSERT INTO users SET ?;`
             result = con.query(sql, post);
-        } catch (error) {
-            
-        } finally {
             con.end();
+        } catch (error) {
+
+        } finally {
         }
+        return result;
     }
     auth = (email, password) => {
 
